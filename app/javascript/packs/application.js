0afdaaf165f1750e5508333172775d1475728b38
Bianca Herbst <b.herbst@posteo.de>
2020-06-09 21:23:59 +0200

    try different carousel, not finished yet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,11 @@ require("channels")
 import "bootstrap";
 import { hoverHome } from './homepage-hover.js';
 import { hoverLogo } from './logo-hover.js';
+import { carouselControl } from './carousel-control.js';
 
 // import { initAutocomplete } from '../plugins/init_mapbox';
 
 
 hoverHome();
 hoverLogo();
+carouselControl();
--- app/javascript/packs/logo-hover.js
+++ app/javascript/packs/logo-hover.js
@@ -2,17 +2,14 @@ const hoverLogo = () => {
   const name = document.getElementsByClassName("header-name");
   if(name){
     name[0].addEventListener('mouseover', () => {
-      console.log("hovering first element")
       name[0].style.opacity = "1";;
       name[1].style.opacity = "1";
     });
     name[0].addEventListener('mouseout', () => {
-      console.log("hovering 2. element")
       name[0].style.opacity = ".8";;
       name[1].style.opacity = ".8";
     });
     name[1].addEventListener('mouseover', () => {
-       console.log("mouseout first element")
       name[0].style.opacity = "1";;
       name[1].style.opacity = "1";
     });
