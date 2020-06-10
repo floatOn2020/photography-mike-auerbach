fa3e1e756f3fbebea16476e99db808ddcb76cabe
Bianca Herbst <b.herbst@posteo.de>
2020-06-10 10:56:26 +0200

    change carousel controls

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,9 @@ require("channels")
 import "bootstrap";
 import { hoverHome } from './homepage-hover.js';
 import { hoverLogo } from './logo-hover.js';
-import { carouselControl } from './carousel-control.js';
 
 // import { initAutocomplete } from '../plugins/init_mapbox';
 
 
 hoverHome();
 hoverLogo();
-carouselControl();
