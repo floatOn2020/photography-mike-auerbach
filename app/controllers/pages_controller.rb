8000135c8c0e4fcd41a3709cc7360db64c453947
Bianca Herbst <b.herbst@posteo.de>
2020-06-23 11:26:46 +0200

    add alert -unsplash-

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    flash[:alert] = "Work in progress. All images taken from 'unsplash.com'."
     render layout: 'home'
   end
 
