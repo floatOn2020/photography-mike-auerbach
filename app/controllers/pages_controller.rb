ddd65d3201a4480f5538d56d555a7cca62666ad7
Bianca Herbst <b.herbst@posteo.de>
2020-06-23 12:35:53 +0200

    refactor carousel-controls

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    flash[:alert] = "Work in progress. All images taken from 'unsplash.com'."
+    flash[:alert] = "Work in progress. Most images taken from 'unsplash.com'."
     render layout: 'home'
   end
 
