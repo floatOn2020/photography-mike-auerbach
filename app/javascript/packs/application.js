9ca877324e9f7d072df5015aba72102c8287872f
Bianca Herbst <b.herbst@posteo.de>
2020-06-29 14:33:46 +0200

    mobile menu logic and some styling --styling not finished

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,11 @@ require("channels")
 import "bootstrap";
 import { cardsHover } from './cards-hover.js';
 import { currentPageLink } from './current-page-link.js';
+import { toggleMenu } from './toggle-mobile-menu.js';
 
 // import { initAutocomplete } from '../plugins/init_mapbox';
 
 
 cardsHover();
 currentPageLink();
+toggleMenu()
--- app/javascript/packs/toggle-mobile-menu.js
+++ app/javascript/packs/toggle-mobile-menu.js
@@ -1,12 +1,30 @@
 const toggleMenu = () => {
-  // const hamburger = document.querySelector(".nav-toggle");
+  const hamburger = document.querySelector(".nav-toggle");
+  const headerNav = document.querySelector('.my-header__nav')
+  const menu = document.querySelector(".my-nav__list");
+  const contentDiv = document.querySelector('.content');
+console.log(menu)
+  const showMenu = () => {
+      if (menu.dataset.show === "true") {
 
-  // hamburger.addEventListener('click', () => {
-  //   let menu = document.querySelector(".my-nav__list");
-  //   console.log('sth')
-  //   menu.style.display = 'none'
-  // })
+        menu.style.transform= "translateX(0%)";
+        headerNav.style.background = '$white';
+      } else {
+        menu.style.transform = "translateX(-100%)";
+        headerNav.style.background = 'transparent';
+        contentDiv.style.marginTop = '105px';
+      }
+  }
 
-};
 
+  if (hamburger) {
+    hamburger.addEventListener('click', (event) => {
+      event.preventDefault();
+      console.log('clicked', 'menu.dataset.show = ', menu.dataset.show )
+      showMenu();
+      menu.dataset.show = menu.dataset.show == 'true' ? 'false' : 'true';
+      console.log('clicked again', 'menu.dataset.show = ', menu.dataset.show)
+    });
+  }
+}
 export { toggleMenu };
