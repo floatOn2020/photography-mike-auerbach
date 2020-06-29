f9f8341ce3d3264e31a0bd4628f815b1094736ad
Bianca Herbst <b.herbst@posteo.de>
2020-06-29 12:14:51 +0200

    adjust logo on home

--- app/javascript/packs/toggle-mobile-menu.js
+++ app/javascript/packs/toggle-mobile-menu.js
@@ -1,11 +1,11 @@
 const toggleMenu = () => {
-  const hamburger = document.querySelector(".nav-toggle");
+  // const hamburger = document.querySelector(".nav-toggle");
 
-  hamburger.addEventListener('click', () => {
-    let menu = document.querySelector(".my-nav__list");
-    console.log('sth')
-    menu.style.display = 'none'
-  })
+  // hamburger.addEventListener('click', () => {
+  //   let menu = document.querySelector(".my-nav__list");
+  //   console.log('sth')
+  //   menu.style.display = 'none'
+  // })
 
 };
 
