c6593c247cdc3a0a7e256e4f256e6264a2ceafef
Bianca Herbst <b.herbst@posteo.de>
2020-07-01 17:53:50 +0200

    navbar working

--- app/javascript/packs/toggle-mobile-menu.js
+++ app/javascript/packs/toggle-mobile-menu.js
@@ -1,41 +1,38 @@
 const toggleMenu = () => {
   const hamburger = document.querySelector(".nav-toggle");
-  const headerNav = document.querySelector('.my-header__nav')
+  const Nav = document.querySelector('.my-nav')
   const menu = document.querySelector(".my-nav__list");
   const contentDiv = document.querySelector('.content');
   const page = document.querySelector('#page-container');
-  const cont = document.querySelector('.cont');
-  // defaults
   if ((window.matchMedia("(max-width: 598px)")).matches && menu) {
-    menu.style.transform = "translateX(-300%)";
+    menu.style.height = '100vh';
     contentDiv.style.marginTop = '105px';
-    headerNav.style.height = 'fit-content';
+    Nav.style.height = '99px';
   } else {
     menu.style.transform = "translateX(0)";
-    headerNav.style.background = '$white';
-    headerNav.style.height = '100%';
+    Nav.style.background = '$white';
     contentDiv.style.marginTop = '2px';
   }
   const showMenu = () => {
     if (menu.dataset.show === "true") {
-      headerNav.style.height = '100%';
-      menu.style.transform= "translateX(0%)";
-      headerNav.style.background = '$white';
-      console.log(headerNav.style.height);
+      menu.style.transition = 'all 1s cubic - bezier(.5, 0, .5, 1)';
+      menu.style.transform = "translateX(0%)";
+      Nav.style.background = '$white';
       page.style.position = 'fixed';
-    } else {
-      menu.style.transform = "translateX(-100%)";
-      headerNav.style.height = 'fit-content';
-      contentDiv.style.marginTop = '105px';
-      page.style.position = 'relative';
-    }
+      } else {
+        contentDiv.style.width = "100vw"
+        menu.style.transform = "translateX(100%)";
+        Nav.style.transition= 'transform 1s';
+        Nav.style.height = '99px';
+        page.style.position = 'relative';
+      }
     }
 
     if (hamburger) {
       hamburger.addEventListener('click', (event) => {
         event.preventDefault();
-        showMenu();
         menu.dataset.show = menu.dataset.show == 'true' ? 'false' : 'true';
+        showMenu();
       });
     }
   }
