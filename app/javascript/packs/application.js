563818e30ee26cf85a19cd826e029f133b3c76c6
Bianca Herbst <b.herbst@posteo.de>
2020-07-23 19:36:07 +0200

    add reload on mobile device, when orientation changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,25 @@
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("@rails/activestorage").start();
+require("channels");
 
 import "bootstrap";
-import { cardsHover } from './cards-hover.js';
-import { currentPageLink } from './current-page-link.js';
-import { toggleMenu } from './toggle-mobile-menu.js';
+import { cardsHover } from "./cards-hover.js";
+import { currentPageLink } from "./current-page-link.js";
+import { toggleMenu } from "./toggle-mobile-menu.js";
 
 // import { initAutocomplete } from '../plugins/init_mapbox';
 
-
 cardsHover();
 currentPageLink();
-toggleMenu()
+toggleMenu();
+
+window.onorientationchange = function () {
+  var orientation = window.orientation;
+  switch (orientation) {
+    case 0:
+    case 90:
+    case -90:
+      window.location.reload();
+      break;
+  }
+};
