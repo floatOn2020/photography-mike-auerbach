785244b3dd25a5e53377fb6bb611169c5afdc6e0
Bianca Herbst <b.herbst@posteo.de>
2020-07-23 23:00:19 +0200

    meta change to hard coded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
-  def default_url_options
-    { host: ENV["DOMAIN"] || "https://mike-auerbach.herokuapp.com/" }
-  end
+  # def default_url_options
+  #   { host: ENV["DOMAIN"] || "https://mike-auerbach.herokuapp.com/" }
+  # end
 end
