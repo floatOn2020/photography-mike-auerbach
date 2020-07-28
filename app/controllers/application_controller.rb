cadf839e6308817aad134b6dc7a90be24577b067
Bianca Herbst <b.herbst@posteo.de>
2020-07-28 11:59:21 +0200

    fix layers (z-index) for shootings show carousel next/prev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,2 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
-  # def default_url_options
-  #   { host: ENV["DOMAIN"] || "https://mike-auerbach.herokuapp.com/" }
-  # end
 end
